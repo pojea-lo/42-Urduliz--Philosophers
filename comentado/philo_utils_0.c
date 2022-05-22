b228947aa0977dfda5bb809df7160f62d7fcd85c
pojea-lo <92981551+pojea-lo@users.noreply.github.com>
2022-05-22 16:03:48 +0200

    Add files via upload

---
+++ comentado/philo_utils_0.c
@@ -0,0 +1,98 @@
+#include "philo.h"
+
+int ft_argm_input (int argc, char *argv[], t_dat *in)
+{
+	int	i;
+
+	in->nph = ft_atoi (argv[1]);
+	in->td = ft_atoi (argv[2]);
+	in->te = ft_atoi (argv[3]);
+	in->ts = ft_atoi (argv[4]);
+	in->ne = (int *) malloc (sizeof(int) * in->nph);
+	if (argc == 5)
+	{
+		i = -1;
+		while (++i < in->nph)
+			in->ne[i] = -3;//pongo -3 pq es menor que 0 y el -1 y -2 esta pillado para errores
+	}
+	else
+	{
+		i = -1;
+		while (++i < in->nph)
+			in->ne[i] = ft_atoi (argv[5]);
+	}
+	if (ft_argm_control (in) == 1)
+		return(1);
+	return(0);
+}
+
+int	ft_argm_control (t_dat *in)
+{
+	if (in->nph == -1 || in->td == -1 || in->te == -1 || in->ts == -1
+	|| in->ne[0] == -1)
+	{
+		write (1, "No number in arguments\n", 23);
+		free (in->ne);
+		return (1);
+	}
+	if (in->nph == -2 || in->td == -2 || in->te == -2 || in->ts == -2
+	|| in->ne[0] == -2)
+	{
+		write (1, "Number exceeded limits\n", 23);
+		free (in->ne);
+		return (1);
+	}
+	return (0);
+}
+
+int	ft_atoi (char *str)
+{
+	int			i;
+	int			c;
+	long int	n;
+
+	c = 0;
+	i = -1;
+	n = 0;
+	while (str[c] != 00)
+		c++;
+	while (++i < c)
+	{
+		if (str[i] < 48 || str[i] > 57)
+			return (-1);
+	}
+	i = -1;
+	while (++i < c)
+		n = (n * 10) + (str[i] - '0');
+	if (n < -2147483648 || n > 2147483647)
+		return (-2);
+	return (n);
+}
+
+void	ft_free_in(t_dat *in)
+{
+	if (in->fork != NULL)
+		free (in->fork);
+	if (in->mutex != NULL)
+		free (in->mutex);
+	if (in->ne != NULL)
+		free (in->ne);
+}
+
+void	ft_free_phil(t_sphilo **phil, pthread_t *t)
+{
+	int	i;
+	int	j;
+
+	i = -1;
+	j = phil[0]->in->nph;
+	if (phil != NULL)
+	{
+		while (++i < j)
+			free (phil[i]);
+		free (phil);
+	}
+	if (t != NULL)
+		free (t);
+}
+
---
+++ envio/philo.h
@@ -0,0 +1,97 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pojea-lo <pojea-lo@student.42urduli>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/22 15:56:20 by pojea-lo          #+#    #+#             */
+/*   Updated: 2022/05/22 15:56:22 by pojea-lo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <pthread.h>
+# include <math.h>
+# include <sys/time.h>
+
+//data input structure
+typedef struct s_dat
+{
+	int				i;
+	int				j;
+	int				k;
+	int				nph;
+	int				*fork;
+	pthread_mutex_t	*mutex;
+	pthread_t		died;
+	pthread_mutex_t	mspf;
+	int				spf;
+	int				sd;
+	int				impi;
+	int				impp;
+	long			td;
+	long			te;
+	long			ts;
+	int				*ne;
+	long			tstart;
+	long			tthink;
+
+}	t_dat;
+
+typedef struct s_sphilo
+{
+	int			id;
+	long		lte;
+	long		lts;
+	int			sp;
+	int			se;
+	int			sem;
+	int			sw;
+	int			ss;
+	t_dat		*in;
+
+}	t_sphilo;
+
+typedef struct s_thread
+{
+	pthread_t	*t;
+}	t_thread;
+
+//philo. (main)c
+int			ft_start(t_dat *in);
+int			ft_start_two(pthread_t *t, t_sphilo **phil);
+t_sphilo	*ft_data(int i, t_dat *in);
+
+//philo_utils_0.c
+int			ft_argm_input(int argc, char *argv[], t_dat *in);
+int			ft_argm_control(t_dat *in);
+int			ft_atoi(char *str);
+void		ft_free_in(t_dat *in);
+void		ft_free_phil(t_sphilo **phil, pthread_t *t);
+
+//philo_utils_1.c
+void		ft_printf(int i, t_sphilo *phil);
+int			ft_check(t_sphilo *phil);
+int			ft_check_moment(t_sphilo *phil);
+long		ft_time(long tstart);
+int			ft_usleep(long time, t_sphilo *phil);
+
+//philo_utils_2.c
+int			ft_eat(t_sphilo *phil);
+int			ft_eat_last(t_sphilo *phil);
+int			ft_sleep(t_sphilo *phil);
+int			ft_think(t_sphilo *phil);
+
+//philo_utils_3.c
+void		*ft_create(void *phil_anterior);
+int			ft_create_aux(t_sphilo *phil);
+int			ft_take(t_sphilo *phil);
+int			ft_take_rest(t_sphilo *phil);
+int			ft_take_last(t_sphilo *phil);
+
+#endif
